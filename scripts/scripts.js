0a615241977a3a09b7f5a6d7444dd5fa43ff2002
Tracey Berg-Fulton <traceybergfulton@gmail.com>
2020-01-04 09:40:22 -0500

    Adding scripts

---
+++ scripts/scripts.js
@@ -0,0 +1,4 @@
+const pickingLanguage = document.querySelector('#language-list').addEventListener('change', (event) => {
+//  console.log(event.target.value);
+ const myValue = event.target.value;
+});
